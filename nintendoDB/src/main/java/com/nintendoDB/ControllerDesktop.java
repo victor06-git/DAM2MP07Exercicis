eb66fc79ebaf53a68286f088dee6ab467a8ab5e0
victor06-git <asensiovector@gmail.com>
2025-09-29 19:22:39 +0200

    .

---
+++ nintendoDB/src/main/java/com/nintendoDB/ControllerDesktop.java
@@ -0,0 +1,51 @@
+package com.nintendoDB;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.utils.UtilsViews;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+
+public class ControllerDesktop implements Initializable{
+
+    @FXML
+    private ChoiceBox<String> choiceTitle;
+
+    @FXML
+    private VBox list;
+
+    @FXML
+    private Label labelNom;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private TextField textFieldDescription;
+}
